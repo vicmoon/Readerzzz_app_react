f8796f6cae8d9d68c2d105975e9e1edd4251ffcd
Munteanu <munteanuvictoria1@gmail.com>
2023-12-13 10:06:42 +0100

    update

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  './globals.css'
+
+const App = () => {
+  return (
+    <h1 className= "bg-white dark:bg-slate-800 rounded-lg px-6 py-8 ring-1 ring-slate-900/5 shadow-xl"> Hello Readerzzz</h1>
+  )
+}
+
+export default App
